32a8b80d00414821f6008479402310e6b5e5e9c2
gmuloc <gmulocher@arista.com>
2022-06-03 15:33:53 +0200

    Test: add test for j2lint.settings.py

---
+++ tests/test_settings.py
@@ -0,0 +1,17 @@
+"""
+Tests for j2lint.settings.py
+"""
+import pytest
+
+from j2lint.settings import settings
+
+
+def test_default_settings():
+    """
+    NOTE: the Settings class should probably be removed from a design point of view
+
+    This test is only here to have the structure
+    """
+    assert settings.verbose is False
+    assert settings.log_level == "info"
+    assert settings.output == "text"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+"""
+Tests for j2lint.utils.py
+"""
 import os
 import pytest
 
