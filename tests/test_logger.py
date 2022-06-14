1b150c76e68125442226e83c1c77493fa79d36cc
gmuloc <gmulocher@arista.com>
2022-06-14 11:48:33 +0200

    ci: add tox to run pytest in CI
    * Fix tests/test_logger.py to by Python 3.6 and 3.7 compliant

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -4,6 +4,7 @@ Tests for j2lint.logger.py
 import logging
 from logging import handlers
 from unittest.mock import create_autospec
+import sys
 
 import pytest
 
@@ -28,9 +29,14 @@ def test_add_handler(logger, stream_handler, log_level):
     add_handler(logger, stream_handler, log_level)
     logger.setLevel.assert_called_once_with(log_level)
     logger.addHandler.assert_called_once()
+
+    # call_args.args was introduced in Python 3.8
+    if sys.version_info >= (3, 8):
+        handler_arg = logger.addHandler.call_args.args[0]
+    elif sys.version_info >= (3, 6):
+        handler_arg = logger.addHandler.call_args[0][0]
+
     if not stream_handler:
-        assert isinstance(
-            logger.addHandler.call_args.args[0], handlers.RotatingFileHandler
-        )
+        assert isinstance(handler_arg, handlers.RotatingFileHandler)
     else:
-        assert isinstance(logger.addHandler.call_args.args[0], logging.StreamHandler)
+        assert isinstance(handler_arg, logging.StreamHandler)
