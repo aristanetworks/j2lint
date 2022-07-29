246b8acffa1ce44eeec27b6ceabf289689488c66
gmuloc <gmulocher@arista.com>
2022-07-29 16:21:02 +0200

    Chore: adjust version to v1.0.0

--- j2lint/__init__.py
+++ j2lint/__init__.py
@@ -1,7 +1,7 @@
 """__init__.py - A command-line utility that checks for best practices in Jinja2.
 """
 NAME = "j2lint"
-VERSION = "1.0.0"
+VERSION = "v1.0.0"
 DESCRIPTION = __doc__
 
 __author__ = "Arista Networks"
