dc149667c899d7eed6f17836c1f41db7b05d5f4d
gmuloc <gmulocher@arista.com>
2026-01-02 09:50:03 +0100

    CI: The Happy New Year 2026 PR

--- j2lint/rules/jinja_variable_has_space_rule.py
+++ j2lint/rules/jinja_variable_has_space_rule.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2025 Arista Networks, Inc.
+# Copyright (c) 2021-2026 Arista Networks, Inc.
 # Use of this source code is governed by the MIT license
 # that can be found in the LICENSE file.
 """jinja_variable_has_space_rule.py - Rule class to check if jinja variables have single space between curly brackets and variable name."""
